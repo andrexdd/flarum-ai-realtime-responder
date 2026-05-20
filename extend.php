dcc4c85798b33fe215915877600b630a796e1497
0xAndreSec <arzenqwee@gmail.com>
2026-05-20 13:57:59 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -5,7 +5,6 @@ use Flarum\Discussion\Event\Started;
 use Andrexdd\AiRealtimeResponder\Listener\SendAiResponse;
 
 return [
-    // Flarum'un yerleşik Event sistemine kanca atıyoruz
     (new Extend\Event())
         ->listen(Started::class, SendAiResponse::class)
 ];
